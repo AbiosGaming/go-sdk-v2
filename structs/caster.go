8851fc4ee1533707672eea559f6154b868c467bd
Patrik Karlström <jp.karlstrom@gmail.com>
2018-12-12 11:18:31 +0100

    Update CasterStruct to reflect that it can be nil

--- structs/caster.go
+++ structs/caster.go
@@ -5,6 +5,6 @@ type CasterStruct struct {
 	Name    string        `json:"name,omitempty"`
 	Type    *int64        `json:"type,omitempty"`
 	Url     string        `json:"url,omitempty"`
-	Stream  StreamStruct  `json:"stream,omitempty"`
+	Stream  *StreamStruct `json:"stream,omitempty"`
 	Country CountryStruct `json:"country,omitempty"`
 }
