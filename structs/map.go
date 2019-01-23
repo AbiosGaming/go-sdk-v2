c40cb3d23d7d367e7a3531f37a9b236e6f17282e
Patrik Karlström <jp.karlstrom@gmail.com>
2019-01-23 12:59:19 +0100

    Add official flag to map

--- structs/map.go
+++ structs/map.go
@@ -2,7 +2,8 @@ package structs
 
 // MapStruct represents the map being played in a Match
 type MapStruct struct {
-	Id   int64      `json:"id"`
-	Name string     `json:"name,omitempty"`
-	Game GameStruct `json:"game,omitempty"`
+	Id       int64      `json:"id"`
+	Name     string     `json:"name,omitempty"`
+	Official bool       `json:"official"`
+	Game     GameStruct `json:"game,omitempty"`
 }
